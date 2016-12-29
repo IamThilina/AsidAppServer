e4a160262227a0ea1915e0470f5566b4a46a3abb
root <root@ubuntu-1gb-blr1-01.localdomain>
2016-12-29 05:20:05 +0000

    route to test Tomcat Server added

--- routes/index.js
+++ routes/index.js
@@ -63,4 +63,32 @@ router.get('/government/test', function(req, res, next) {
     http.request(options, callback).end();
 });
 
+router.get('/tomcat/test', function(req, res, next) {
+
+    var options = {
+        host: 'localhost',
+        port: 8080,
+        path: '/',
+        method: 'GET'
+    };
+
+    callback = function(response) {
+        var str = '';
+
+        //another chunk of data has been recieved, so append it to `str`
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        //the whole response has been recieved, so we just print it out here
+        response.on('end', function () {
+		console.log(str);
+            	res.send(str);
+        });
+    };
+
+    http.request(options, callback).end();
+});
+
+
 module.exports = router;
