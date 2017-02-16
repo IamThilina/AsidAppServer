8848ff02fd1e5f4da1b4489d55426f867a655ecc
IamThilina <333thilina@gmail.com>
2017-02-16 19:41:21 +0530

    Testing System With SocialMedia Profiles Only

--- client/app/services/search.service.ts
+++ client/app/services/search.service.ts
@@ -14,7 +14,7 @@ export class SearchService {
 
     search(user: FormData): Promise<any> {
         const headers = new Headers({'Content-Type': 'application/json'});
-        const url = `${this.API_URL}/search`;
+        const url = `${this.API_URL}/search-social`;
         return new Promise((resolve, reject) => {
             //noinspection TypeScriptUnresolvedFunction
             this.http
