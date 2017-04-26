8fa2c79acfa2fa4336f6a551ff3722be7357ee67
IamThilina <333thilina@gmail.com>
2017-04-26 20:01:45 +0530

    Revert "Testing System With SocialMedia Profiles Only"
    This reverts commit 8848ff02fd1e5f4da1b4489d55426f867a655ecc.

--- client/app/services/search.service.ts
+++ client/app/services/search.service.ts
@@ -14,7 +14,7 @@ export class SearchService {
 
     search(user: FormData): Promise<any> {
         const headers = new Headers({'Content-Type': 'application/json'});
-        const url = `${this.API_URL}/search-social`;
+        const url = `${this.API_URL}/search`;
         return new Promise((resolve, reject) => {
             //noinspection TypeScriptUnresolvedFunction
             this.http
